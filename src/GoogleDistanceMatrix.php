8a7ee6112b03a14c8ceb3b64e771536aa7892d5d
Martin Lutonský <martin.lutonsky@gmail.com>
2019-01-14 10:12:37 +0100

    fixed setDestination local variable assignment
    missing 's' in variable name

--- src/GoogleDistanceMatrix.php
+++ src/GoogleDistanceMatrix.php
@@ -287,7 +287,7 @@ class GoogleDistanceMatrix
      */
     public function setDestination($destination) : GoogleDistanceMatrix
     {
-        $this->destination = array($destination);
+        $this->destinations = array($destination);
         return $this;
     }
 
